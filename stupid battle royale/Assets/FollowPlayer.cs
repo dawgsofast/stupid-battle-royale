d751aaf7a1eb33844a6b451f4e6a7d99f2b440a6
dawgsofast <meltriss5445@gmail.com>
2024-11-25 10:38:13 +0100

    added camera follow to the player
    its kinda broken right now but it will work for now, will fix later

---
+++ stupid battle royale/Assets/FollowPlayer.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    // Get the position of the playerModel
+    public Transform cameraPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
